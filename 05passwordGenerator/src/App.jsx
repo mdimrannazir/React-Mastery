7c83420e2d30e65cc10597260ad262c55bba6d81
mdimrannazir <imrannazir1.in@gmail.com>
2024-03-14 19:32:41 +0600

    Random Password Generator V1.1

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,11 +1,13 @@
-import { useEffect } from 'react'
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 
 function App() {
   const [length, setLength] = useState(8)
   const [numbersAllowed, setNumbersAllowed] = useState(false)
   const [charectersAllowed, setCharecterAllowed] = useState(false)
   const [password, setPassword] = useState("")
+  const [btnText, setBtnText] = useState("Copy")
+  // useRef hook
+  const passwordRef = useRef(null)
 
   const passwordGenerator = useCallback(() => {
     let pass = ""
@@ -23,34 +25,48 @@ function App() {
 
   }, [length, numbersAllowed, charectersAllowed, setPassword])
 
+  const copyPassToClipboard = useCallback(() => {
+    setBtnText("Copied")
+    passwordRef.current?.select()
+    passwordRef.current?.setSelectionRange(0, 99)
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
   useEffect(() => {
+    setBtnText("Copy")
     passwordGenerator()
   }, [length, numbersAllowed, charectersAllowed, passwordGenerator])
+
+
   return (
     <>
       <div className='bg-gray-700 px-10 py-16 my-12 mx-96 rounded-xl text-gray-600'>
-        <div className='px-12'>
+        <div className='px-24'>
           <h1 className='text-center font-bold text-4xl text-white mb-8'>Random Passoword Generator</h1>
-          <div className='flex rounded-xl'>
+          <div className='flex rounded-xl overflow-hidden'>
             <input
               type="text"
-              className='w-full rounded-xl shadow-xl font-bold text-4xl outline-none py-1 px-3'
+              className='w-full shadow-xl font-bold text-4xl outline-none py-1 px-3'
               placeholder='Password'
               readOnly
               value={password}
+              ref={passwordRef}
             />
-            <button className='rounded-xl ml-1 bg-blue-700 px-5 py-0.5 text-white font-bold outline-none'>Copy</button>
+            <button
+              className='bg-blue-700 px-5 py-0.5 text-white font-bold outline-none'
+              onClick={copyPassToClipboard}
+            >{btnText}</button>
           </div>
           <div className='mt-6 font-bold text-white gap-8 flex text-xl'>
             <label>
               <input type="range"
                 min={6}
-                max={50}
+                max={30}
                 className='cursor-pointer h-4'
                 value={length}
                 onChange={(e) => { setLength(e.target.value) }}
               />
-              {length} Length</label>
+              Length: {length}</label>
 
             <label className='cursor-pointer'>
               <input type="checkbox"
