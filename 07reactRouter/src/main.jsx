9490b52c5e8679580b2f0aaeaf28cc83a92754d0
mdimrannazir <imrannazir1.in@gmail.com>
2024-03-21 16:18:45 +0600

    reconnecting gihub check

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -8,27 +8,27 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: '',
-        element: <Home />
-      },
-      {
-        path: 'about',
-        element: <About />
-      },
-      {
-        path: 'contact',
-        element: <Contact />
-      }
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: '',
+//         element: <Home />
+//       },
+//       {
+//         path: 'about',
+//         element: <About />
+//       },
+//       {
+//         path: 'contact',
+//         element: <Contact />
+//       }
 
-    ]
-  }
-])
+//     ]
+//   }
+// ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
